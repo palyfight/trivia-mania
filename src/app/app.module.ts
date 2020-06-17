3db690325d2fcd16bdd22ce265147530dde6b323
mmoua <mmouafo@hotmail.com>
2020-06-17 17:13:37 -0400

    1- changer TriviaName pour img et moved dans assets, une image a ete ajoutee 2- install packages bootstrap + jquery, mettre les paths dans angular.json 3- creer un folder Components et bouger les components dedans + changer les path dans app.module (pour lisibilite lol) 4- ajouter new components footer, nav, leaderboard (qui seront dans home-page)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TrivaButtonComponent } from './triva-button/triva-button.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { MenuComponent } from './menu/menu.component';
+import { TrivaButtonComponent } from './Components/triva-button/triva-button.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { MenuComponent } from './Components/menu/menu.component';
 import {RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
-import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { LeaderboardComponent } from './Components/leaderboard/leaderboard.component';
 
 const appRoutes: Routes = [
   {path: 
@@ -30,7 +31,8 @@ const appRoutes: Routes = [
     RegistrationComponent,
     MenuComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
