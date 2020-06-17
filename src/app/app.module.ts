c7e49f4229f6de87b6fd1187daa52a9d5f3b72e1
mmoua <mmouafo@hotmail.com>
2020-06-16 21:10:11 -0400

    components pour home sont a priori creer et on a import Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrivaButtonComponent } from './triva-button/triva-button.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { MenuComponent } from './menu/menu.component';
+import {RouterModule, Routes } from '@angular/router'
+
+const appRoutes: Routes = [
+  {path: 
+    '**',
+    component : HomePageComponent
+   }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrivaButtonComponent
+    TrivaButtonComponent,
+    HomePageComponent,
+    LoginComponent,
+    RegistrationComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
