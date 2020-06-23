2eafb555d397627a66b929a2bd777a72e75f0cbc
Christian Bamatembera <chrisbama92@gmail.com>
2020-06-23 18:38:50 -0400

    completed register form! Thx Phil xD

--- src/app/Components/registration/registration.component.ts
+++ src/app/Components/registration/registration.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+enum State {
+  Login,
+  Registration,
+  Forgot
+}
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +23,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  loginState: State;
 
-  ngOnInit(): void {
+  constructor() {
+    this.loginState = State.Registration;
   }
 
+  openDialogLogin() {
+    this.loginState = State.Login;
+  }
+
+  ngOnInit(): void {}
 }
